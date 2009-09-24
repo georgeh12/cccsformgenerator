e7ea536f2e89c7f852d2e8a6af322d9225a658b6
georgehardigg@gmail.com <georgehardigg@gmail.com@6691ae1a-a0dc-11de-b40a-0bcb3bc6a7b4>
2009-09-24 16:30:58 +0000

    window adapter

--- trunk/CCCS Form Generator/src/cccs/utility/MinimizeToTray.java
+++ trunk/CCCS Form Generator/src/cccs/utility/MinimizeToTray.java
@@ -6,11 +6,19 @@
 package cccs.utility;
 
 import java.awt.event.*;
+import java.awt.*;
 
 /**
  *
  * @author ghardigg
  */
 public class MinimizeToTray extends WindowAdapter {
-    
+    private boolean closing = false;
+    private Component active_frame = null;
+
+    public void windowClosing(WindowEvent evt){
+        if(!closing){
+            active_frame = evt.getComponent();
+        }
+    }
 }
