a6e38ecf1b67d44d86c024c9634cbe166177ce92
georgehardigg@gmail.com <georgehardigg@gmail.com@6691ae1a-a0dc-11de-b40a-0bcb3bc6a7b4>
2009-09-24 16:18:32 +0000

    window adapter

---
+++ CCCS Form Generator/src/cccs/utility/MinimizeToTray.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cccs.utility;
+
+import java.awt.event.*;
+
+/**
+ *
+ * @author ghardigg
+ */
+public class MinimizeToTray extends WindowAdapter {
+    
+}
